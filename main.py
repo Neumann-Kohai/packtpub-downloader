7e25105cd5c1d3507f3186eba50082c264651cfe
Neumann-kohai <contact@neumann-kohai.de>
2021-12-15 15:38:06 +0100

    switch to argparse and some small changes

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import os
 import sys
 import glob
 import math
-import getopt
+import argparse
 import requests
 from tqdm import tqdm, trange
 from config import BASE_URL, PRODUCTS_ENDPOINT, URL_BOOK_TYPES_ENDPOINT, URL_BOOK_ENDPOINT
 from user import User
 
 
-#TODO: I should do a function that his only purpose is to request and return data
+# TODO: I should do a function that his only purpose is to request and return data
 def book_request(user, offset=0, limit=10, verbose=False):
     data = []
     url = BASE_URL + PRODUCTS_ENDPOINT.format(offset=offset, limit=limit)
@@ -23,6 +23,7 @@ def book_request(user, offset=0, limit=10, verbose=False):
     data += r.json().get('data', [])
 
     return url, r, data
+
 
 def get_books(user, offset=0, limit=10, is_verbose=False, is_quiet=False):
     '''
@@ -35,12 +36,12 @@ def get_books(user, offset=0, limit=10, is_verbose=False, is_quiet=False):
             how many book wanna get by request
     '''
     # TODO: given x time jwt expired and should refresh the header, user.refresh_header()
-    
+
     url, r, data = book_request(user, offset, limit)
-    
+
     print(f'You have {str(r.json()["count"])} books')
     print("Getting list of books...")
-    
+
     if not is_quiet:
         pages_list = trange(r.json()['count'] // limit, unit='Pages')
     else:
@@ -55,17 +56,17 @@ def get_url_book(user, book_id, format='pdf'):
     '''
         Return url of the book to download
     '''
-    
+
     url = BASE_URL + URL_BOOK_ENDPOINT.format(book_id=book_id, format=format)
     r = requests.get(url, headers=user.get_header())
 
-    if r.status_code == 200: # success
+    if r.status_code == 200:  # success
         return r.json().get('data', '')
 
-    elif r.status_code == 401: # jwt expired 
-        user.refresh_header() # refresh token 
-        get_url_book(user, book_id, format)  # call recursive 
-    
+    elif r.status_code == 401:  # jwt expired
+        user.refresh_header()  # refresh token
+        get_url_book(user, book_id, format)  # call recursive
+
     print('ERROR (please copy and paste in the issue)')
     print(r.json())
     print(r.status_code)
@@ -80,13 +81,13 @@ def get_book_file_types(user, book_id):
     url = BASE_URL + URL_BOOK_TYPES_ENDPOINT.format(book_id=book_id)
     r = requests.get(url, headers=user.get_header())
 
-    if  (r.status_code == 200): # success
+    if (r.status_code == 200):  # success
         return r.json()['data'][0].get('fileTypes', [])
-    
-    elif (r.status_code == 401): # jwt expired 
-        user.refresh_header() # refresh token 
-        get_book_file_types(user, book_id, format)  # call recursive 
-    
+
+    elif (r.status_code == 401):  # jwt expired
+        user.refresh_header()  # refresh token
+        get_book_file_types(user, book_id, format)  # call recursive
+
     print('ERROR (please copy and paste in the issue)')
     print(r.json())
     print(r.status_code)
@@ -98,13 +99,13 @@ def download_book(filename, url):
     '''
         Download your book
     '''
-    print('Starting to download ' + filename)
+    print(f"Starting to download {filename.split('/')[-1]}")
 
     with open(filename, 'wb') as f:
         r = requests.get(url, stream=True)
         total = r.headers.get('content-length')
         if total is None:
-            f.write(response.content)
+            f.write(r.content)
         else:
             total = int(total)
             # TODO: read more about tqdm
@@ -112,7 +113,7 @@ def download_book(filename, url):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
                     f.flush()
-            print('Finished ' + filename)
+            print(f"Finished {filename.split('/')[-1]}")
 
 
 def make_zip(filename):
@@ -142,62 +143,37 @@ def does_dir_exist(directory):
             sys.exit(2)
 
 
-def main(argv):
+def main():
     # thanks to https://github.com/ozzieperez/packtpub-library-downloader/blob/master/downloader.py
-    email = None
-    password = None
-    root_directory = 'media' 
-    book_file_types = ['pdf', 'mobi', 'epub', 'code']
-    separate = None
-    verbose = None
-    quiet = None
-    errorMessage = 'Usage: main.py -e <email> -p <password> [-d <directory> -b <book file types> -s -v -q]'
 
-    # get the command line arguments/options
-    try:
-        opts, args = getopt.getopt(
-            argv, 'e:p:d:b:svq', ['email=', 'pass=', 'directory=', 'books=', 'separate', 'verbose', 'quiet'])
-    except getopt.GetoptError:
-        print(errorMessage)
-        sys.exit(2)
+    parser = argparse.ArgumentParser(
+        usage="Usage: main.py -e <email> -p <password> [-d <directory> -b <book file types> -s -v -q]")
+    parser.add_argument('-e', '--email', help='Your email', required=True)
+    parser.add_argument('-p', '--password',
+                        help='Your password', required=True)
+    parser.add_argument('-d', '--directory',
+                        help='Directory to save your books', required=False, dest="root_directory", type=lambda d: os.path.expanduser(d) if '~' in d else os.path.abspath(d))
+    parser.add_argument('-b', '--book-file-types', help='Book file types to download',
+                        required=False, choices=("pdf", "mobi", "epub", "code"), nargs='*', default="pdf")
 
-    # hold the values of the command line options
-    for opt, arg in opts:
-        if opt in ('-e', '--email'):
-            email = arg
-        elif opt in ('-p', '--pass'):
-            password = arg
-        elif opt in ('-d', '--directory'):
-            root_directory = os.path.expanduser(
-                arg) if '~' in arg else os.path.abspath(arg)
-        elif opt in ('-b', '--books'):
-            book_file_types = arg.split(',')
-        elif opt in ('-s', '--separate'):
-            separate = True
-        elif opt in ('-v', '--verbose'):
-            verbose = True
-        elif opt in ('-q', '--quiet'):
-            quiet = True
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose', help='Verbose',
+                       required=False, action='store_true')
+    group.add_argument('-q', '--quiet', help='Quiet',
+                       required=False, action='store_true')
 
-    if verbose and quiet:
-        print("Verbose and quiet cannot be used together.")
-        sys.exit(2)
+    parser.add_argument('-s', '--separate', help='Separate books by type',
+                        required=False, action='store_true')
 
-    # do we have the minimum required info?
-    if not email or not password:
-        print(errorMessage)
-        sys.exit(2)
-
-    # check if not exists dir and create
-    does_dir_exist(root_directory)
+    args = parser.parse_args()
 
     # create user with his properly header
-    user = User(email, password)
-
+    user = User(args.email, args.password)
+    print(args)
     # get all your books
-    books = get_books(user, is_verbose=verbose, is_quiet=quiet)
+    books = get_books(user, is_verbose=args.verbose, is_quiet=args.quiet)
     print('Downloading books...')
-    if not quiet:
+    if not args.quiet:
         books_iter = tqdm(books, unit='Book')
     else:
         books_iter = books
@@ -205,22 +181,24 @@ def main(argv):
         # get the different file type of current book
         file_types = get_book_file_types(user, book['productId'])
         for file_type in file_types:
-            if file_type in book_file_types:  # check if the file type entered is available by the current book
-                book_name = book['productName'].replace(' ', '_').replace('.', '_').replace(':', '_').replace('/','')
-                if separate:
-                    filename = f'{root_directory}/{book_name}/{book_name}.{file_type}'
-                    move_current_files(root_directory, book_name)
+            # check if the file type entered is available by the current book
+            if file_type in args.book_file_types:
+                book_name = book['productName'].replace(' ', '_').replace(
+                    '.', '_').replace(':', '_').replace('/', '')
+                if args.separate:
+                    filename = f'{args.root_directory}/{book_name}/{book_name}.{file_type}'
+                    move_current_files(args.root_directory, book_name)
                 else:
-                    filename = f'{root_directory}/{book_name}.{file_type}'
+                    filename = f'{args.root_directory}/{book_name}.{file_type}'
                 # get url of the book to download
                 url = get_url_book(user, book['productId'], file_type)
                 if not os.path.exists(filename) and not os.path.exists(filename.replace('.code', '.zip')):
                     download_book(filename, url)
                     make_zip(filename)
                 else:
-                    if verbose:
+                    if args.verbose:
                         tqdm.write(f'{filename} already exists, skipping.')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
